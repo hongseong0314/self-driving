cc6b56fe9f77fcf76a25e952d81e70cefa700a7a
DESKTOP-GDAM1P2\user1229 <mi70107010@gmail.com>
2022-03-17 18:24:30 +0900

    2part bird eye view and sliding window commit

--- hong/2_part/test.py
+++ hong/2_part/test.py
@@ -5,9 +5,56 @@ import os
 from glob import glob
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-from utils import line_dectection
+from _2part_utils import processed_frames, compute_offset_from_center, prepare_out_blend_frame, binarize, birdeye, get_fits_by_previous_fits, get_fits_by_sliding_windows
+from _2part_utils import Line, draw_back_onto_the_road
 from calibration import undistort
+
+import pickle
+with open(os.path.join(os.path.dirname(os.path.abspath("HONG")), './camera_matrix.pickle'), 'rb') as f:
+    data = pickle.load(f)
+mtx = data['mtx']
+dist = data['dist']
+
+# init line state
+line_lt, line_rt = Line(buffer_len=10), Line(buffer_len=10)
+
+def process_pipeline(frame, keep_state=True):
+    """
+    Apply whole lane detection pipeline to an input color frame.
+    :param frame: input color frame
+    :param keep_state: if True, lane-line state is conserved (this permits to average results)
+    :return: output blend with detected lane overlaid
+    """
+
+    global line_lt, line_rt, processed_frames
+
+    # undistort the image using coefficients found in calibration
+    img_undistorted = undistort(frame, mtx, dist, img_plot=False)
+
+    # binarize the frame s.t. lane lines are highlighted as much as possible
+    img_binary = binarize(img_undistorted, verbose=False)
+
+    # compute perspective transform to obtain bird's eye view
+    img_birdeye, M, Minv = birdeye(img_binary, verbose=False)
+
+    # fit 2-degree polynomial curve onto lane lines found
+    if processed_frames > 0 and keep_state and line_lt.detected and line_rt.detected:
+        line_lt, line_rt, img_fit = get_fits_by_previous_fits(img_birdeye, line_lt, line_rt, verbose=False)
+    else:
+        line_lt, line_rt, img_fit = get_fits_by_sliding_windows(img_birdeye, line_lt, line_rt, n_windows=9, verbose=False)
+
+    # compute offset in meter from center of the lane
+    offset_meter = compute_offset_from_center(line_lt, line_rt, frame_width=frame.shape[1])
+
+    # draw the surface enclosed by lane lines back onto the original frame
+    blend_on_road = draw_back_onto_the_road(img_undistorted, Minv, line_lt, line_rt, keep_state)
+
+    # stitch on the top of final output images from different steps of the pipeline
+    blend_output = prepare_out_blend_frame(blend_on_road, img_binary, img_birdeye, img_fit, line_lt, line_rt, offset_meter)
+
+    processed_frames += 1
+
+    return blend_output
 
 def camload(w, h, path, avi=False):
     """카메라 켜기"""
@@ -42,15 +89,12 @@ def camload(w, h, path, avi=False):
         ret, frame = capture.read()
         if not ret:
             continue
-        # edge_img = edge_dec(frame)
-        # mask_img = region_mask(edge_img)
-        calibration_img = undistort(frame, mtx, dist)
-        blend_img = line_dectection(frame)
-        cal_blen_img = line_dectection(calibration_img)
-        cv2.imshow("frame", blend_img)
-        cv2.imshow("calibration", cal_blen_img)
+
+        pre_img = process_pipeline(frame, keep_state=False)
+
+        cv2.imshow("frame", pre_img)
         if avi:
-            out.write(blend_img)
+            out.write(pre_img)
 
     return capture, frame
 
@@ -68,7 +112,7 @@ if __name__ == "__main__":
     path = os.path.join(data_dir, '../data/original_vid.mp4')
     w = 1280
     h = 720
-
+    
     cam, img = camload(w, h, path=path, avi=False)
     # img_save(img, r"hong/test1.png")
     cam_close(cam)
